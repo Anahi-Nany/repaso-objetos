f249ceea9102ac66b494a61a54f091704719e92e
Anahi-Nany <anahi.roclop@gmail.com>
2022-05-27 21:07:58 -0500

    FINAL

---
+++ src/main/java/uaslp/objetos/figuras/Cuadrado.java
@@ -0,0 +1,32 @@
+package uaslp.objetos.figuras;
+
+public class Cuadrado extends Figura{
+
+    private double lado;
+    private double area;
+
+    public Cuadrado(double lado){
+        super("Cuadrado");
+        this.lado = lado;
+    }
+    public Cuadrado(){
+        super("Cuadrado");
+    }
+//setters
+    public void setLado(double lado){
+        this.lado=lado;
+    }
+//getters
+    public double getArea(){
+        area = lado*lado;
+        if(lado == 0){
+            throw new LadoNoProvistoException();
+        }
+        return area;
+    }
+    public double getLado(){
+        return lado;
+    }
+
+
+}
